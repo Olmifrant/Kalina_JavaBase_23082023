13a85a892ad95f08e8a24ac5139645290b568177
Kalina <olmifrant@gmail.com>
2023-09-08 14:16:07 +0200

    src

---
+++ src/main/java/ua/hillel/kalina/lessons/Less_002/Main_less_002.java
@@ -0,0 +1,18 @@
+package ua.hillel.kalina.lessons.Less_002;
+
+public class Main_less_002 {
+
+    public static void main(String[] args) {
+
+        double longitude = 40.689978;
+        double latitude = 74.04544;
+        char specSymbol = '\u00B0';
+        char specSymbol2 = '\'';
+        System.out.println("Statue of Liberty coordinates for alien attack");
+        System.out.println("longtitude: " + longitude + " latittude: - " + latitude);
+        System.out.println("\"DMS Lat\": 40" + specSymbol + " 41" + specSymbol2 + "23.92\" N  \"DMS Long\": -74" + specSymbol + "02" + specSymbol2 + "43.61\" W ");
+
+
+
+    }
+}
---
+++ src/main/java/ua/hillel/kalina/lessons/less_001/Main.java
@@ -0,0 +1,7 @@
+package ua.hillel.kalina.lessons.less_001;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
