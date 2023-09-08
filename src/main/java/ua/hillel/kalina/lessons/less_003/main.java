4aad4f22ac239add70bb6719095bd96e1e2e09f9
Kalina <olmifrant@gmail.com>
2023-09-08 15:52:29 +0200

    Less_003_DZ

--- src/main/java/ua/hillel/kalina/lessons/less_003/main.java
+++ src/main/java/ua/hillel/kalina/lessons/less_003/main.java
@@ -2,6 +2,7 @@ package ua.hillel.kalina.lessons.less_003;
 
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("Hello GitHub");
+        System.out.println("Everything is working");
     }
 }
