d5e6ca5e7a7e1476d6e569d359fb2d6362a9239b
Kalina <olmifrant@gmail.com>
2023-09-08 17:32:26 +0200

    new_settings_git

--- src/main/java/ua/hillel/kalina/lessons/less_003/main.java
+++ src/main/java/ua/hillel/kalina/lessons/less_003/main.java
@@ -4,5 +4,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("Everything is working");
+        System.out.println("did everything again");
+        System.out.println("Everything is working");
     }
 }
