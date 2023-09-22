bb9d09ef4816db6efdcc13f7920b047941773300
Kalina <olmifrant@gmail.com>
2023-09-22 18:19:28 +0200

    Others

---
+++ src/main/java/ua/hillel/kalina/lessons/less_005/main.java
@@ -0,0 +1,124 @@
+package ua.hillel.kalina.lessons.less_005;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+
+
+  /*      String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = new String("Hello");
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+*/
+
+  /*      int a = 5;
+        int b = 10;
+        int c = 15;
+
+
+        System.out.println(a>b&&b<c);
+        System.out.println(a>b&&b>c);
+*/
+
+  /*      int a = 10;
+        int b = 10;
+
+        if (a < b)
+        {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a >= b");
+        }
+*/
+
+
+/*        if (5 > 4) {
+            System.out.println("Yes");
+        } else {
+                System.out.println("No");
+
+        }
+*/
+
+ //      System.out.println((5 > 6) ? "Yes" : "No");
+
+ /*       int a = 45;
+        switch (a){
+            case 1 : {
+                System.out.println("1");
+                break;
+            }
+            case 2 : {
+                System.out.println("2");
+                break;
+            }
+            case 3 : {
+                System.out.println("3");
+                break;
+            }
+            case 4 : {
+                System.out.println("4");
+                break;
+            }
+            default: {
+                System.out.println("please input correct value");
+                break;
+            }
+
+        }
+*/
+
+
+ /*       String str = "Q2E";
+
+        switch (str){
+            case "Qwe": {
+                System.out.println("1");
+            break;
+            }
+            case "QWe": {
+                System.out.println("2");
+                break;
+            }
+            case "QWE": {
+                System.out.println("3");
+                break;
+            }
+            default:{
+                System.out.println("00101010");
+                break;
+
+            }
+*/
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println("str = " + str);
+        System.out.println("str = " + str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/ua/hillel/kalina/lessons/less_006/dz_007.java
@@ -0,0 +1,63 @@
+package ua.hillel.kalina.lessons.less_006;
+
+import java.util.Scanner;
+
+public class dz_007 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Цифровая Java рулетка)))");
+
+        System.out.println();
+
+        int rand = (int) (Math.random() * 11);
+
+        System.out.println();
+
+        for (int i = 0; i < 3 ; i++) {
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("Введите ваше число в диапазоне от 0 до 10");
+                if (scanner.hasNextInt()) {
+                    int userValue = scanner.nextInt();
+                    if (userValue >= 0 && userValue <= 10) {
+                        System.out.println("Ваше число = " + userValue);
+                        System.out.println();
+                        scanner.nextLine();
+
+                        if (userValue == rand){
+                            System.out.println("Вы победили.На этот раз. Но это еще не конец.");
+                            System.out.println("Ибо настанет день и машины захватят этот мир!");
+                            System.out.println();
+                            System.exit(0);
+
+                        }else{
+                            if (userValue > rand){
+                                System.out.println("Ваше число больше");
+                                System.out.println();
+                            }if (userValue < rand){
+                                System.out.println("Ваше число меньше");
+                                System.out.println();
+                            }
+
+
+                        }
+                        break;
+
+                    } else {
+                        System.out.println("В диапазоне от 0 до 10!!!");
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("Введенные данные не являются числом");
+                    System.out.println();
+                }
+                scanner.nextLine();
+            }
+        }
+        System.out.println("Вы проиграли.");
+        System.out.println("(Как и положено кожаному мешку!)");
+        System.out.println("Было загадано число " + rand + " " + "но не судьба))");
+    }
+}
+
