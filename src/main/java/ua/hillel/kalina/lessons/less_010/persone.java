83faf1edc537d26aeb95b74a5dd2b9505033af96
Kalina <olmifrant@gmail.com>
2023-10-06 14:11:25 +0200

    dz_plus

--- src/main/java/ua/hillel/kalina/lessons/less_010/persone.java
+++ src/main/java/ua/hillel/kalina/lessons/less_010/persone.java
@@ -4,17 +4,17 @@ public class persone {
 
     public static void main(String[] args) {
 
-    personeInfo("Alex","kalina","Zapor","380638084228");
-    personeInfo("Alex","kalina","Zapor","380638084228");
-    personeInfo("Alex","kalina","Zapor","380638084228");
-    personeInfo("Alex","kalina","Zapor","380638084228");
+        personeInfo("Dean", "Winchester", "Lawrence", " 1-866-907-3235");
+        personeInfo("Samuel", "Winchester", "Lawrence", "1-806-307-0741");
+        personeInfo("John", "Winchester", "Lawrence", "1-855-723-6997");
+        personeInfo("Castiel", "(Cas)", "Heaven", "Angel Radio");
+        personeInfo("Fergus", "MacLeod", "Hell", "Rite at the crossroads");
 
     }
 
-    static void personeInfo(String firstName,String lastName,String sity,String number) {
+    static void personeInfo(String firstName, String lastName, String sity, String number) {
         System.out.print("Позвонить персонажу " + " " + firstName + " ");
         System.out.println(lastName + " " + "живет в городе " + sity + " " + "можно по номеру " + number);
 
     }
-
 }
