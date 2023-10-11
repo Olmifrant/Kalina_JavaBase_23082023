68da10dfe5954dd215a8b4c82fd8ae1bf0892f47
Kalina <olmifrant@gmail.com>
2023-10-11 20:06:47 +0200

    DZ

--- src/main/java/ua/hillel/kalina/lessons/less_011/BurgersMain.java
+++ src/main/java/ua/hillel/kalina/lessons/less_011/BurgersMain.java
@@ -13,6 +13,9 @@ public class BurgersMain {
         Burger burger3 = new Burger(true, true, true, true, true);
         Burger burger4 = new Burger(false);
 
+        System.out.println();
+        System.out.println("При выборе любого варианта зубочистка в подарок!");
+
 //        System.out.println();
 //        System.out.println(burger1);
 //        System.out.println(burger2);
@@ -24,6 +27,7 @@ public class BurgersMain {
 
     class Burger {
 
+        int count = 2;
         private boolean bun;
         private boolean meat;
         private boolean cheese;
@@ -38,8 +42,9 @@ public class BurgersMain {
             this.lettuce = true;
             this.mayonnaise = true;
 
-            System.out.println("1. Стандартный бургер. Состав: булка, мясо, сыр, зелень, майонез");
-
+            System.out.println("1. Стандартный бургер.");
+            System.out.println("Состав: булка, мясо, сыр, зелень, майонез");
+            System.out.println();
         }
 
         public Burger(boolean bun, boolean meat, boolean cheese, boolean lettuce) {
@@ -50,13 +55,15 @@ public class BurgersMain {
             this.lettuce = lettuce;
             this.mayonnaise = false;
 
-            System.out.println("2. Диетический бургер. Состав: булка, мясо, сыр, зелень, без майонеза");
-
+            System.out.println("2. Диетический бургер.");
+            System.out.println("Состав: булка, мясо, сыр, зелень, без майонеза");
+            System.out.println();
         }
 
         public Burger(boolean bun, boolean meat, boolean cheese, boolean lettuce, boolean mayonnaise) {
 
-            int count = 2;
+
+            this.count = count;
             this.bun = bun;
             this.meat = meat;
             this.cheese = cheese;
@@ -64,7 +71,9 @@ public class BurgersMain {
             this.mayonnaise = mayonnaise;
 
             if (count == 2) {
-                System.out.println("3. Счастливый  бургер. Состав: булка, мясо двойное, сыр, зелень, майонез");
+                System.out.println("3. Счастливый  бургер.");
+                System.out.println("Состав: булка, мясо двойное, сыр, зелень, майонез");
+                System.out.println();
             } else {
                 System.out.println("Обязательна двойная порция мяса");
             }
@@ -78,11 +87,11 @@ public class BurgersMain {
             this.lettuce = false;
             this.mayonnaise = false;
 
-            System.out.println("4. Бургер без булки, мяса, сыра, зелени и майонеза.");
-            System.out.println("      Супер акция! Два бургера по цене трех!");
+            System.out.println("4. Бургер Обещания депутата");
+            System.out.println("Состав: пустота + ничто");
+            System.out.println("При выборе этого варианта действует СУПЕР АКЦИЯ! Два бургера по цене трех!");
 
         }
-
 
     }
 
