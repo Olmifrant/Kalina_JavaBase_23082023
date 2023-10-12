209ad37f53e498228684420be19bb9337cf9ab15
Kalina <olmifrant@gmail.com>
2023-10-12 14:59:36 +0200

    DZ

--- src/main/java/ua/hillel/kalina/lessons/less_011/BurgersMain.java
+++ src/main/java/ua/hillel/kalina/lessons/less_011/BurgersMain.java
@@ -8,19 +8,19 @@ public class BurgersMain {
         System.out.println("В нашем меню доступно четыре вида бургеров");
         System.out.println();
 
-        Burger burger1 = new Burger();
+        Burger burger1 = new Burger(true, true);
         Burger burger2 = new Burger(true, true, true, true);
         Burger burger3 = new Burger(true, true, true, true, true);
-        Burger burger4 = new Burger(false);
+        Burger burger4 = new Burger();
 
         System.out.println();
         System.out.println("При выборе любого варианта зубочистка в подарок!");
 
-        System.out.println();
-        System.out.println(burger1);
-        System.out.println(burger2);
-        System.out.println(burger3);
-        System.out.println(burger4);
+//        System.out.println();
+//        System.out.println(burger1);
+//        System.out.println(burger2);
+//        System.out.println(burger3);
+//        System.out.println(burger4);
 
     }
 }
@@ -30,20 +30,22 @@ public class BurgersMain {
         int count = 2;
         boolean bun;
         boolean meat;
+        boolean meatX2;
         boolean cheese;
         boolean lettuce;
         boolean mayonnaise;
 
-        public Burger() {
+        public Burger(boolean bun, boolean meat) {
 
             this.bun = true;
             this.meat = true;
+            this.meatX2 = false;
             this.cheese = true;
             this.lettuce = true;
             this.mayonnaise = true;
 
             System.out.println("1. Стандартный бургер.");
-            System.out.println("Состав: булка, мясо, сыр, зелень, майонез");
+            System.out.println("Состав: булка, мясо, сыр, зелень, майонез.");
             System.out.println();
         }
 
@@ -51,45 +53,48 @@ public class BurgersMain {
 
             this.bun = bun;
             this.meat = meat;
+            this.meatX2 = false;
             this.cheese = cheese;
             this.lettuce = lettuce;
             this.mayonnaise = false;
 
             System.out.println("2. Диетический бургер.");
-            System.out.println("Состав: булка, мясо, сыр, зелень, без майонеза");
+            System.out.println("Состав: булка, мясо, сыр, зелень, без майонеза.");
             System.out.println();
         }
 
-        public Burger(boolean bun, boolean meat, boolean cheese, boolean lettuce, boolean mayonnaise) {
+        public Burger(boolean bun, boolean meatX2, boolean cheese, boolean lettuce, boolean mayonnaise) {
 
 
 
             this.bun = bun;
-            this.meat = meat;
+            this.meat = false;
+            this.meatX2 = meatX2;
             this.cheese = cheese;
             this.lettuce = lettuce;
             this.mayonnaise = mayonnaise;
 
             if (count == 2) {
                 System.out.println("3. Счастливый  бургер.");
-                System.out.println("Состав: булка, мясо двойное, сыр, зелень, майонез");
+                System.out.println("Состав: булка, мясо двойное, сыр, зелень, майонез.");
                 System.out.println();
             } else {
-                System.out.println("3. Бургер с двойной порцией мяса временно недоступен для выбора");
+                System.out.println("3.Извините  бургер с двойной порцией мяса временно недоступен для заказа.");
                 System.out.println();
             }
         }
 
-        public Burger(boolean bun) {
+        public Burger() {
 
             this.bun = false;
             this.meat = false;
+            this.meatX2 = false;
             this.cheese = false;
             this.lettuce = false;
             this.mayonnaise = false;
 
-            System.out.println("4. Бургер Обещания депутата");
-            System.out.println("Состав: пустота + ничто");
+            System.out.println("4. Бургер Обещания депутата.");
+            System.out.println("Состав: пустота + ничто.");
             System.out.println("При выборе этого варианта действует СУПЕР АКЦИЯ! Два бургера по цене трех!");
 
         }
