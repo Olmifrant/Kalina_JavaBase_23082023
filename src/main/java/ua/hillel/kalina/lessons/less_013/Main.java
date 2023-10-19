e6c1c583d8ef81074967830b258111bd35960844
Kalina <olmifrant@gmail.com>
2023-10-19 11:40:35 +0200

    DZ_014

--- src/main/java/ua/hillel/kalina/lessons/less_013/Main.java
+++ src/main/java/ua/hillel/kalina/lessons/less_013/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MusicStyles[] musicStyles= {new PopMusic(),new ClassicMusic(),new RockMusic()};
+        MusicStyles[] musicStyles= {new PopMusic(),new ClassicMusic(),new RockMusic(),new PostRock()};
 
         System.out.println();
 
